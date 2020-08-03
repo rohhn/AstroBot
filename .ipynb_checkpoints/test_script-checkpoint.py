2540e6822d2556579fd1027857ae5b40c47c42b0
Rohan <rohan@MacBook-Pro.local>
2020-08-03 20:37:01 +0530

    Added functionality for keyword search

---
+++ .ipynb_checkpoints/test_script-checkpoint.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+from bs4 import BeautifulSoup as bs4
+import requests
+import json
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import datetime
+import calendar
+
+topics=['astronomy', 'latest+astronomy+news', 'astronomy+events','black+hole', 'space+exploration', 'hubble+space+telescope','space+observatory']
+
+
+# In[2]:
+
+
+def scrape(url):
+    page  = requests.get(url)
+    page_text = bs4(page.text,'html.parser')
+    articles_text = page_text.find_all(class_ = "ZINbbc xpd O9g5cc uUPGi")
+    return articles_text
+
+
+# In[3]:
+
+
+def get_url(url):
+    #
+    url = url.find('a').attrs['href'].replace('/url?q=','')
+    url = url.split("&sa")[0]
+    return url
+
+
+# In[4]:
+
+
+def get_time(i):
+    posted_time = i.find(class_ = "r0bn4c rQMQod").contents[0]
+    posted_time = posted_time.split(" ")
+    if (posted_time[1] == 'hours' or posted_time[1] == 'hour'):
+        time_since = 0.1 * int(posted_time[0])
+    elif (posted_time[1] == 'days' or posted_time[1] == 'day'):
+        time_since = 1 * int(posted_time[0])
+    else:
+        time_since = 999
+    return(time_since)
+
+
+# In[5]:
+
+
+def get_final_article(articles_text):
+    data = []
+    for i in articles_text: 
+        data.append([get_url(i),get_time(i),i.find(class_ = "BNeawe vvjwJb AP7Wnd").contents[0]])
+    data = sorted(data, key = lambda x: x[1])
+    return(data[0][0])
+
+
+# In[6]:
+
+
+def get_article(keyword):
+    
+    #day = datetime.datetime.now().strftime("%A")
+    #time = int(datetime.datetime.now().strftime("%H"))
+    #if day == 'Sunday' and time == 16:
+    #    keyword = topics[0]
+    #elif day == 'Wednesday' and time == 16:
+    #    keyword = topics[1]
+    #elif day == 'Friday' and time == 16:
+    #    keyword = topics[2]
+        
+    url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjZzKWTjv3qAhVFyzgGHeKzCf8Q_AUoAXoECBUQAw&biw=1680&bih=947"
+    articles_text = scrape(url)
+    return(get_final_article(articles_text))
+
+
+# In[ ]:
+
+
+def weekly_article(update, context):
+    #get_article()
+    chat_id = update.message.chat_id
+    #update.message.reply_text(get_article())
+    #bot.send_message(chat_id=chat_id, text=get_article())
+    update.message.reply_text(get_article(str(context.args)))
+
+def main():
+    #day = datetime.datetime.now().strftime("%A")
+    #time = int(datetime.datetime.now().strftime("%H"))
+    
+    updater = Updater('1183471904:AAENQORzTAU_mTDXfv8xJU1s3rmK1PuzlpU', use_context=True)
+    dp = updater.dispatcher
+    #dp.add_handler(CommandHandler('randomarticle',random_article))
+    dp.add_handler(CommandHandler('about', weekly_article, pass_args=True))
+    #dp.add_handler(MessageHandler(~Filters.command & Filters.text, test))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
--- main_bot.py
+++ main_bot.py
@@ -6,14 +6,11 @@ import random
 import re
 
 
-#Topics for random article generator
-random_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events','black+holes', 'space+exploration', 'hubble+space+telescope','space+observatory']
-
-#topics for 3 weekly articles
-weekly_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events']
 
 
 
+
+# --------------------- HELPER FUNCTIONS ------------------------#
 def scrape(url):
     #parse html text of the google news page url
     page  = requests.get(url)
@@ -27,10 +24,6 @@ def get_article_url(url):
     url = url.find('a').attrs['href'].replace('/url?q=','')
     url = url.split("&sa")[0]
     return url
-
-
-# In[174]:
-
 
 def get_time(i):
     posted_time = i.find(class_ = "r0bn4c rQMQod").contents[0]
@@ -47,9 +40,14 @@ def get_time(i):
         time_since = 9999
     return(time_since)
 
+# ---------------------- HELPER FUNCTIONS ------------------------#
 
-# In[175]:
 
+
+#GENERATE RANDOM ARTICLES FROM POOL OF TOPICS
+
+#Topics for random article generator
+random_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events','black+holes', 'space+exploration', 'hubble+space+telescope','space+observatory']
 
 def get_final_random_article(articles_text):
     data = []
@@ -59,6 +57,24 @@ def get_final_random_article(articles_text):
     i = random.randrange(0,len(articles_text),1)
     return(data[i][0])
 
+def get_random_article():
+    i = random.randrange(0,len(random_topics),1)
+    keyword = random_topics[i]
+    url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjTi4TOw_7qAhWzyDgGHVjpAyYQ_AUoAXoECBUQAw&biw=1680&bih=948"
+    articles_text = scrape(url)
+    return(get_final_random_article(articles_text))
+
+#BOT CALLED FUNCTION
+def random_article(update, context):
+    chat_id = update.message.chat_id
+    update.message.reply_text(get_random_article())
+
+
+#GENERATE 3 ARTICLES PER WEEK (SUNDAY, WEDNESDAY, FRIDAY)
+
+#topics for 3 weekly articles
+weekly_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events']
+
 def get_final_weekly_article(articles_text):
     data = []
     for i in articles_text: 
@@ -67,24 +83,14 @@ def get_final_weekly_article(articles_text):
     return(data[0][0])
 
 
-# In[176]:
-
-
-def get_random_article():
-    i = random.randrange(0,len(random_topics),1)
-    keyword = random_topics[i]
-    url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjTi4TOw_7qAhWzyDgGHVjpAyYQ_AUoAXoECBUQAw&biw=1680&bih=948"
-    articles_text = scrape(url)
-    return(get_final_random_article(articles_text))
-
 def get_weekly_article():
     day = datetime.datetime.now().strftime("%A")
     time = int(datetime.datetime.now().strftime("%H"))
-    if day == 'Monday': #and time == 13:
+    if day == 'Monday' and time == 13:
         keyword = weekly_topics[0]
-    elif day == 'Wednesday': #and time == 16:
+    elif day == 'Wednesday' and time == 16:
         keyword = weekly_topics[1]
-    elif day == 'Friday': #and time == 16:
+    elif day == 'Friday' and time == 16:
         keyword = weekly_topics[2]
     else:
         return("No article today.")
@@ -93,25 +99,35 @@ def get_weekly_article():
     articles_text = scrape(url)
     return(get_final_weekly_article(articles_text))
 
-def random_article(update, context):
-    #get_article()
-    chat_id = update.message.chat_id
-    #update.message.reply_text(get_article())
-    #bot.send_message(chat_id=chat_id, text=get_article())
-    update.message.reply_text(get_random_article())
+#BOT CALLED FUNCTION
+#def weekly_article(update, context):
+#    chat_id = update.message.chat_id
+#    update.message.reply_text(get_weekly_article())
 
-def weekly_article(update, context):
-    #get_article()
+
+
+#FETCH ARTICLE BASED ON KEYWORDS BY USER
+def get_keyword_article(keyword):
+    url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjTi4TOw_7qAhWzyDgGHVjpAyYQ_AUoAXoECBUQAw&biw=1680&bih=948"
+    articles_text = scrape(url)
+    return(get_final_weekly_article(articles_text))
+
+def fetch_article(update, context):
     chat_id = update.message.chat_id
-    #update.message.reply_text(get_article())
-    #bot.send_message(chat_id=chat_id, text=get_article())
-    update.message.reply_text(get_weekly_article())
+    update.message.reply_text(get_keyword_article(str(context.args)))
+    
+#calls same function as weekly article generator    
+    
+
 
 def main():
+    #day = datetime.datetime.now().strftime("%A")
+    #time = int(datetime.datetime.now().strftime("%H"))
+    
     updater = Updater('1183471904:AAENQORzTAU_mTDXfv8xJU1s3rmK1PuzlpU', use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('randomarticle',random_article))
-    dp.add_handler(CommandHandler('newarticle', weekly_article))
+    dp.add_handler(CommandHandler('newarticle', fetch_article, pass_args=True))
     #dp.add_handler(MessageHandler(~Filters.command & Filters.text, test))
     updater.start_polling()
     updater.idle()
