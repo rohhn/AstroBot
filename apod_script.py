7a90b3631b8642cbfebc0e694712830ddf13291d
Rohan <rohan@MacBook-Pro.local>
2020-08-04 09:59:25 +0530

    apod bug fixes

--- apod_script.py
+++ apod_script.py
@@ -4,28 +4,40 @@ import requests
 from better_profanity import profanity
 import smtplib
 import time
+import hashlib
 
 while(True):
     url = "https://apod.nasa.gov/apod/astropix.html"
     response = requests.get(url)
     page_text = bs4(response.text, 'html.parser')
     
-    old_date = page_text.find("p").contents[3].get_text().replace('\n','')
-    old_image_url = "https://apod.nasa.gov/apod/"+page_text.find_all('center')[0].find('img')['src']
-    old_title = page_text.find_all('center')[1].get_text().replace("\n","")
+    #old_hash = hashlib.sha224(page_text).hexdigest()
+    old_response = requests.get(url)
+    old_page_text = bs4(old_response.text, 'html.parser')
+    old_date = old_page_text.find("p").contents[3].get_text().replace('\n','')
+    old_image_url = "https://apod.nasa.gov/apod/"+old_page_text.find_all('center')[0].find('img')['src']
+    old_title = old_page_text.find_all('center')[1].get_text().replace("\n","")
+
+    print(time.time())
+    time.sleep(86400)
     
-    time.sleep(60)
-    
-    new_date = page_text.find("p").contents[3].get_text().replace('\n','')
-    new_image_url = "https://apod.nasa.gov/apod/"+page_text.find_all('center')[0].find('img')['src']
-    new_title = page_text.find_all('center')[1].get_text().replace("\n","")
-    
+    #new_hash = hashlib.sha224(page_text).hexdigest()
+    new_response = requests.get(url)
+    new_page_text = bs4(new_response.text, 'html.parser')
+    new_date = new_page_text.find("p").contents[3].get_text().replace('\n','')
+    new_image_url = "https://apod.nasa.gov/apod/"+new_page_text.find_all('center')[0].find('img')['src']
+    new_title = new_page_text.find_all('center')[1].get_text().replace("\n","")
+
 
     if(old_date == new_date):
         print("no change")
         continue
     else:
-        msg = "updated"
+        
+        #new_date = page_text.find("p").contents[3].get_text().replace('\n','')
+        #new_image_url = "https://apod.nasa.gov/apod/"+page_text.find_all('center')[0].find('img')['src']
+        #new_title = page_text.find_all('center')[1].get_text().replace("\n","")
+        msg = "apod updated"
         toad = ['deshmukh.rohan06@gmail.com', 'singh.s.lakshya@gmail.com']
         fromad = 'deshmukhr849@gmail.com'
         
