2d67cb31697e36291fdca0b7ee564519c7d1584d
Rohan <rohan@MacBook-Pro.local>
2020-08-06 14:54:21 +0530

    new and improved apod script

--- .ipynb_checkpoints/apod_script-checkpoint.py
+++ .ipynb_checkpoints/apod_script-checkpoint.py
@@ -4,8 +4,11 @@ import time
 from telegram import Bot
 from datetime import datetime
 import pytz
+import hashlib
+import re
 
 bot = Bot("1163369796:AAE1BI447fKiuDQ9RUTCUZKcS7-Ek96zlYI") #photobot
+group_id = "-1001331038106" #"-1001284948052"
 
 def get_url(page_text):
     if(page_text.find('img')):
@@ -14,56 +17,41 @@ def get_url(page_text):
     elif(page_text.find('iframe')):
         url = page_text.find_all('center')[0].find('iframe')['src']
         return url
-        
+    
+def get_title(page_text):
+    if(page_text.find('img')):
+        return(re.sub("  +","",page_text.find_all('center')[1].get_text().replace("\n","").split("Image")[0]))
+    elif(page_text.find('iframe')):
+        return(re.sub("  +","",page_text.find_all('center')[1].get_text().replace("\n","").split("Video")[0]))
+    
+def sendmessage(response):
+    page_text = bs4(response.text,'html.parser')
+    date = page_text.find("p").contents[3].get_text().replace('\n','')
+    url = get_url(page_text)
+    title = get_title(page_text)
+    explanation = page_text.find_all('p')[2].get_text().replace("\n","").split("Tomorrow")[0].split("Explanation: ")[1]
+    if(page_text.find('img')):
+        bot.sendPhoto(chat_id=group_id, photo=url, caption = str("APOD: "+title+"\n\n"+explanation+"\nDate: " + date))
+    elif(page_text.find('iframe')):
+        message = ("<a href=\""+url+"\">" +"<b>Astronomy Picture of the Day</b></a>\n\n" +"\n" +explanation+"\n<i>Date: "+date+"</i>\n")
+        bot.sendMessage(chat_id=group_id, text=message, parse_mode='HTML')
 
 while(True):
     url = "https://apod.nasa.gov/apod/astropix.html"
-    response = requests.get(url)
-    page_text = bs4(response.text, 'html.parser')
     
     old_response = requests.get(url)
-    old_page_text = bs4(old_response.text, 'html.parser')
-    old_date = old_page_text.find("p").contents[3].get_text().replace('\n','')
-    #old_image_url = get_url(old_page_text)
-    #old_title = old_page_text.find_all('center')[1].get_text().replace("\n","")
-
-    #print(time.time())
+    old_hash = hashlib.md5(old_response.text.encode('utf-8')).hexdigest()
+    
     bot.sendMessage(chat_id="1045695336", text= "Going to sleep")
-    time.sleep(3600)
+    time.sleep(10)
     
     new_response = requests.get(url)
-    new_page_text = bs4(new_response.text, 'html.parser')
-    new_date = new_page_text.find("p").contents[3].get_text().replace('\n','')
-    new_image_url = get_url(new_page_text)
-    new_title = new_page_text.find_all('center')[1].get_text().replace("\n","")
-    new_explanation = new_page_text.find_all('p')[2].get_text().replace("\n","").split("Tomorrow")[0]
+    new_hash = hashlib.md5(new_response.text.encode('utf-8')).hexdigest()
 
-
-    if(old_date == new_date):
-        bot.sendMessage(chat_id="1045695336", text= "no change")
-        #bot.sendPhoto(chat_id="-1001331038106", photo=old_image_url, caption=str(old_title +"\nPicture taken on: " + old_date + "\n\n\ncourtesy of PhotoBot"))
+    if(old_hash == new_hash):
+        bot.sendMessage(chat_id="1045695336", text= "no change") #personal chat message
         continue
     else:
-        if(page_text.find('img')):
-            bot.sendPhoto(chat_id="-1001284948052", photo=new_image_url, caption = str(new_title +"\nPicture taken on: " + new_date + "\n\n\ncourtesy of PhotoBot"))
-        elif(page_text.find('iframe')):
-            message = ("<a href=\""+new_image_url+"\">" +"<b>Astronomy Picture of the Day</b></a>\n\n" +"\n" +new_explanation+"\n<i>Date: "+new_date+"</i>\n")
-            bot.sendMessage(chat_id="-1001284948052", text=message, parse_mode='HTML')
+        sendmessage(new_response)
         bot.sendMessage(chat_id="-1001331038106", text= ("Updated at: " + str(datetime.now().astimezone(pytz.timezone('Asia/Kolkata')))))
-        #bot.sendMessage()
         break
-        
-#def main():
-#    #day = datetime.datetime.now().strftime("%A")
-#    #time = int(datetime.datetime.now().strftime("%H"))
-#    
-#    updater = Updater('1183471904:AAENQORzTAU_mTDXfv8xJU1s3rmK1PuzlpU')
-#    dp = updater.dispatcher
-#    dp.add_handler(CommandHandler('wiki',get_wiki_info))
-#    #dp.add_handler(MessageHandler(~Filters.command & Filters.text, test))
-#    updater.start_polling()
-#    updater.idle()
-#
-#if __name__ == '__main__':
-#    main()
-#
