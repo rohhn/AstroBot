01b8921f58b2851ea05ecb9749591f9a75436c36
Rohan <rohan@MacBook-Pro.local>
2020-08-06 17:52:56 +0530

    fetch_picture script added

--- .ipynb_checkpoints/test_script-checkpoint.py
+++ .ipynb_checkpoints/test_script-checkpoint.py
@@ -1,10 +1,48 @@
-from telegram import Bot, Update
+from bs4 import BeautifulSoup as bs4
+import requests
+import time
+from telegram import Bot
 from datetime import datetime
 import pytz
-from datetime import timezone
+import hashlib
+import re
+from telegram.ext import Updater, CommandHandler
+from better_profanity import profanity
 
-#time.astimezone(pytz.timezone('Asia/Kolkata')).strftime("%H")
+token = "1163369796:AAE1BI447fKiuDQ9RUTCUZKcS7-Ek96zlYI"
+#bot = Bot(token) #photobot
+group_id = "-1001331038106" #"-1001284948052"
 
-bot = Bot("1222703294:AAEnBuUwV4H8GSv-h2e3Jgfv77QMqpTRsVc")
-msg="time now: " + str(datetime.now().astimezone(pytz.timezone('Asia/Kolkata')))
-bot.sendMessage(chat_id="-1001331038106",text=msg)
+
+def fetch_picture(update, context):
+    chat_id = update.message.chat_id
+    search_text=""
+    for i in context.args:
+  #     if(profanity.contains_profanity(i)):
+  #         update.message.reply_text("no result")
+  #         break
+  #     else:
+        search_text = search_text + " " + i
+    search_text = search_text + "+astronomy+picture"
+    url="http://www.google.com/search?q="+ search_text +"&rlz=1C5CHFA_enIN908IN908&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjkxYHbzYTrAhVTSX0KHZSHDvYQ_AUoAXoECA4QAw&biw=1261&bih=914&safe=active"
+    page_text = requests.get(url)
+    page_text = bs4(page_text.text, 'html.parser')
+    image_url = page_text.find_all(class_ ="TxbwNb")[0].find('img')['src']
+    update.message.reply_photo(image_url)
+    
+
+def main():
+    
+    updater = Updater(token, use_context=True)
+    
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('picture',fetch_picture))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
