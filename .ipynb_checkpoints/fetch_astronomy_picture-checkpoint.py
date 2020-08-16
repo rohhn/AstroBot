14314a2a7e5cc664e0e86be17b4620df0ba116ca
Rohan <rohan@MacBook-Pro.local>
2020-08-16 10:50:55 +0530

    fetch picture updated

--- .ipynb_checkpoints/fetch_astronomy_picture-checkpoint.py
+++ .ipynb_checkpoints/fetch_astronomy_picture-checkpoint.py
@@ -9,32 +9,35 @@ import re
 from telegram.ext import Updater, CommandHandler
 from better_profanity import profanity
 
-token = "1163369796:AAGmNm8peqmRL7Bf9LPwg5RYsqDfs9LHbrQ"
-#bot = Bot(token) #photobot
-group_id = "-1001331038106" #"-1001284948052"
+token = "1183471904:AAHzW9eC9XIHJwJXRiyRKrJemA3WVxY_mug"
 
 
+    
 def fetch_picture(update, context):
-    chat_id = update.message.chat_id
     search_text=""
     for i in context.args:
-  #     if(profanity.contains_profanity(i)):
-  #         update.message.reply_text("no result")
-  #         break
-  #     else:
         search_text = search_text + " " + i
-    search_text = search_text + "+astronomy+picture"
-    url="http://www.google.com/search?q="+ search_text +"&rlz=1C5CHFA_enIN908IN908&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjkxYHbzYTrAhVTSX0KHZSHDvYQ_AUoAXoECA4QAw&biw=1261&bih=914&safe=active"
-    page_text = requests.get(url)
-    page_text = bs4(page_text.text, 'html.parser')
-    image_url = page_text.find_all(class_ ="TxbwNb")[0].find('img')['src']
-    update.message.reply_photo(image_url)
+    search_text += " astronomy"
+    api_key="c999b9b0-df71-11ea-bf62-cf2a6e7f12ba"
+
+    headers = { 'apikey': api_key }
+
+    params = (
+       ("q",search_text),
+       ("tbm","isch"),
+       ("device","desktop"),
+       ("hl","en"),
+       ("location","Manhattan,New York,United States"),
+    )
+    response = requests.get('https://app.zenserp.com/api/v2/search', headers=headers, params=params)
+    images_dict = response.json()
+    image_url = images_dict["image_results"][0]['sourceUrl']
+    context.bot.sendPhoto(chat_id= update.message.chat_id, photo = image_url)
     
 
 def main():
     
     updater = Updater(token, use_context=True)
-    
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('picture',fetch_picture))
     updater.start_polling()
