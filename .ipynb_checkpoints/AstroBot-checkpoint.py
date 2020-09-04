5833ac2f14c95aae66d0062898f64a922b4ed269
Rohan <rohan@MacBook-Pro.local>
2020-09-04 23:41:18 +0530

    weather feature

--- .ipynb_checkpoints/AstroBot-checkpoint.py
+++ .ipynb_checkpoints/AstroBot-checkpoint.py
@@ -9,8 +9,9 @@ from better_profanity import profanity
 from telegram import Bot
 import pytz
 import time
+import sys
 
-token = '1222703294:AAFtKTZoWytkkt9ZUFehhbwuUrYyzzlitUU'  #"1183471904:AAHzW9eC9XIHJwJXRiyRKrJemA3WVxY_mug" 
+token ="1183471904:AAHzW9eC9XIHJwJXRiyRKrJemA3WVxY_mug"  #'1222703294:AAFtKTZoWytkkt9ZUFehhbwuUrYyzzlitUU'  #
 bot = Bot(token)
 ind_tz = pytz.timezone('Asia/Kolkata')
 
@@ -182,7 +183,7 @@ def scrape_wiki(search_text):
             text = summary_text + "\n\n" + wiki_link
             return(text)
         except:
-            contex.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(scrape_wiki):\n" + str(sys.exc_info()))
+            context.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(scrape_wiki):\n" + str(sys.exc_info()))
             return("Cannot find Wikipedia page.")
             
 
@@ -207,20 +208,22 @@ def get_moon_info():
     moon_phase = td_response.findAll('section',{'class':'bk-focus'})[0].find('a').text
     return moon_image, moon_percent, moon_phase
 
-def openweather(lat, lon):
-    openweather_api = "90701b1aba6e661af014c16e653b91c3"
-    openweather_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely&units=metric"+"&appid="+openweather_api
+def weather_data(lat, lon):
+    openweather_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&exclude=minutely&units=metric"+"&appid=90701b1aba6e661af014c16e653b91c3"
     openweather_response = (requests.get(openweather_url)).json()
-    latitude = openweather_response['lat']
-    longitude = openweather_response['lon']
+    clearoutside_image = "https://clearoutside.com/forecast_image_medium/"+str(round(lat,2))+"/"+str(round(lon,2))+"/forecast.png"
     sunset_time = datetime.datetime.fromtimestamp(int(openweather_response['current']['sunset'])).time()
     cloud_cover = openweather_response['current']['clouds']
     wind_speed = round(float(openweather_response['current']['wind_speed']*18/5),2)
     description = openweather_response['current']['weather'][0]['description']
+    dew_point  = openweather_response['current']['dew_point']
+    temperature  = openweather_response['current']['temp']
     icon_url = "http://openweathermap.org/img/wn/"+openweather_response['current']['weather'][0]['icon']+"@2x.png"
     moon_image, moon_percent, moon_phase = get_moon_info()
-    message = "Weather update for "+str(latitude)+", "+str(longitude)+ "\nCloud cover: "+str(cloud_cover)+"%\nWind speed: "+str(wind_speed)+"kmph\nStatus: "+description+"\nMoon Illumination: "+moon_percent+"\nMoon phase: "+moon_phase
-    return message, moon_image
+    #"Weather update for "+str(lat)+", "+str(lon)+ 
+    html_message = "Weather update for <b>"+str(round(lat,2))+", "+str(round(lon,2))+"\nStatus:</b> "+description+"\n<b>Cloud cover: </b>"+str(cloud_cover)+"%\n<b>Wind speed:</b> "+str(wind_speed)+"kmph\n<b>Dew Point:</b> "+str(dew_point)+"°C\n<b>Temperature:</b> "+str(temperature)+"°C\n<b>Moon Illumination:</b> "+moon_percent+"\n<b>Moon phase:</b> "+moon_phase
+    message = "Weather update for "+str(round(lat,2))+", "+str(round(lon,2))+"\nStatus: "+description+"\nCloud cover: "+str(cloud_cover)+"%\nWind speed: "+str(wind_speed)+"kmph\nTemperature: "+str(temperature)+"°C\nDew Point: "+str(dew_point)+"°C\nMoon Illumination: "+moon_percent+"\nMoon phase: "+moon_phase
+    return message, moon_image, clearoutside_image
     
 
 def get_weather(update, context):
@@ -228,12 +231,19 @@ def get_weather(update, context):
     for i in context.args:
         search_text += i
     #bot.sendMessage(chat_id=update.message.chat_id, text = search_text)    
-    lat, lon = search_text.replace(' ','').split(',')
-    try: 
-        message, photo = openweather(lat, lon)
-        context.bot.sendPhoto(chat_id=update.message.chat_id, caption = message, photo=photo)
+    if(re.search("^[0-9]",search_text)):
+        lat, lon = search_text.replace(' ','').split(',')
+    else:
+        geolocation_url = "https://dev.virtualearth.net/REST/v1/Locations?key=Al3NnfvA47J04pxm1b6YfknCea0TYqx4TuzYQJ_EnCXTb5N8ZLMwPtrB631UHiJJ&o=json&q="+search_text+"&jsonso="+search_text
+        geolocation_response = (requests.get(geolocation_url)).json()
+        lat, lon = geolocation_response['resourceSets'][0]['resources'][0]['point']['coordinates']
+    try:    
+        message, moon_photo, cl_image = weather_data(lat, lon)
+        context.bot.sendPhoto(chat_id=update.message.chat_id, caption = message, photo=cl_image)
+        #context.bot.sendMessage(chat_id="-1001331038106", text = str(lat) + ", "+str(lon))
     except:
-        context.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(get_weather):\n" + str(sys.exc_info()))
+        context.bot.sendMessage(chat_id=update.message.chat_id, text="Error in retrieving data.")
+        context.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(scrape_wiki):\n" + str(sys.exc_info()))
 
 # ----------------------------------------------------------------------------------------------#
 
@@ -241,7 +251,7 @@ def get_weather(update, context):
 # ------------------------------------ PRINT HELP COMMANDS -------------------------------------#
 def bot_help(update, context):
     chat_id = update.message.chat_id
-    help_text = "Hello, these are the commands I will respond to:\n\nTyping \"/randomarticle\" will fetch a random article related to an astronomy subject.\n\nTyping \"/newarticle <keyword>\" will fetch an article related to the keyword.\n\nTyping \"/wiki <keyword>\" will produce a short summary and link to wikipedia\n\nTyping \"/weather <latitude, longitude>\" will fetch a weather update.\n\nTyping \"/help\" will show you all the current list of commands I can respond to."
+    help_text = "Hello, these are the commands I will respond to:\n\nTyping \"/randomarticle\" will fetch a random article related to an astronomy subject.\n\nTyping \"/newarticle <keyword>\" will fetch an article related to the keyword.\n\nTyping \"/wiki <keyword>\" will produce a short summary and link to wikipedia\n\nTyping \"/weather <latitude, longitude>\" or \"/weather <location name>\" will fetch a weather update.\n\nTyping \"/help\" will show you all the current list of commands I can respond to."
     update.message.reply_text(help_text)
     
 # ----------------------------------------------------------------------------------------------#
