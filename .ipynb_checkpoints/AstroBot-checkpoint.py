a216154854e12efbe40946817affea987cb04e53
Rohan <rohan@MacBook-Pro.local>
2020-09-13 01:11:04 +0530

    organised repo

--- .ipynb_checkpoints/AstroBot-checkpoint.py
+++ .ipynb_checkpoints/AstroBot-checkpoint.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from bs4 import BeautifulSoup as bs4
 import requests
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters
@@ -6,33 +5,27 @@ import datetime
 import random
 import re
 from better_profanity import profanity
-from telegram import Bot
 import pytz
 import time
 import sys
-from streamlit import caching
 
-token = '1222703294:AAFtKTZoWytkkt9ZUFehhbwuUrYyzzlitUU'  #"1183471904:AAHzW9eC9XIHJwJXRiyRKrJemA3WVxY_mug"  
-bot = Bot(token)
+
+token = '1222703294:AAFtKTZoWytkkt9ZUFehhbwuUrYyzzlitUU'
 ind_tz = pytz.timezone('Asia/Kolkata')
 
 
 # --------------------- HELPER FUNCTIONS ------------------------#
 def scrape(url):
-    #parse html text of the google news page url
-    page  = requests.get(url)
-    page_text = bs4(page.text,'html.parser')
+    page  = requests.get(url) 
+    page_text = bs4(page.text,'html.parser') # parse google news page to find all articles
     articles_text = page_text.find_all(class_ = "ZINbbc xpd O9g5cc uUPGi")
-    #create list of articles
-    return articles_text
+    return articles_text #return list of all articles
 
 def get_article_url(url):
-    #
-    url = url.find('a').attrs['href'].replace('/url?q=','')
-    url = url.split("&sa")[0]
+    url = url.find('a').attrs['href'].replace('/url?q=','').split('&sa')[0]
     return url
 
-def get_time(i):
+def get_time(i): 
     posted_time = i.find(class_ = "r0bn4c rQMQod").contents[0]
     posted_time = posted_time.split(" ")
     if (posted_time[1] == 'mins' or posted_time[1] == 'min'):
@@ -43,19 +36,15 @@ def get_time(i):
         time_since = 24*60 * int(posted_time[0])
     elif (posted_time[1] == 'weeks' or posted_time[1] == 'week'):
         time_since = 7*24*60* int(posted_time[0])
-    #elif (posted_time[1] == 'months' or posted_time[1] == 'month'):
-    #    time_since = 100* int(posted_time[0])
     else:
         time_since = 9999999
-    return(time_since)
+    return(time_since) #return time since posted
 
-# ---------------------- HELPER FUNCTIONS ------------------------#
+# ---------------------- END HELPER FUNCTIONS ------------------------#
 
 
 
 # ------------------- GENERATE RANDOM ARTICLES FROM POOL OF TOPICS ----------------------#
-
-#Topics for random article generator
 
 
 def get_final_random_article(articles_text):
@@ -67,7 +56,7 @@ def get_final_random_article(articles_text):
     return(data[i][0])
 
 def get_random_article():
-    random_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events','black+holes', 'space+exploration', 'hubble+space+telescope','space+observatory','astrophysics', 'Cosmology', 'Astrophotography']
+    random_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events','black+holes', 'space+exploration', 'hubble+space+telescope','space+observatory','astrophysics', 'Cosmology', 'Astrophotography'] #Topics for random article generator
     i = random.randint(0,len(random_topics)-1)
     keyword = random_topics[i]
     url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjTi4TOw_7qAhWzyDgGHVjpAyYQ_AUoAXoECBUQAw&biw=1680&bih=948"
@@ -86,8 +75,6 @@ def random_article(update, context):
 
 # ------------- GENERATE 3 ARTICLES PER WEEK (SUNDAY, WEDNESDAY, FRIDAY) ---------------- #
 
-
-
 def get_final_article(articles_text):
     data = []
     for i in articles_text: 
@@ -98,28 +85,20 @@ def get_final_article(articles_text):
 def send_article(context):
     weekly_article_topics=['astronomy', 'latest+astronomy+news', 'astronomy+events','space+observations']
     day = datetime.datetime.now().astimezone(ind_tz).strftime("%A")
-    #context.bot.sendMessage(chat_id = context.job.context,text = day)
-    #time = int(datetime.datetime.now().strftime("%H"))
-   # try:
-    #if day == 'Sunday':
-    #    topic_day = weekly_article_topics[0]
-    #elif day == 'Wednesday':
-    #    topic_day = weekly_article_topics[1]
-    #elif day == 'Friday':
-    #    topic_day = weekly_article_topics[2]
-    topic_day = weekly_article_topics[random.randint(0,len(weekly_article_topics)-1)]
+    if day == 'Sunday':
+        topic_day = 'the+sky+this+week+site:astronomy.com'  #Astronomy magazine weekly update
+    else:
+        topic_day = weekly_article_topics[random.randint(0,len(weekly_article_topics)-1)] #Random article from pool of topics
+    
     url = "https://www.google.com/search?q="+topic_day+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjZzKWTjv3qAhVFyzgGHeKzCf8Q_AUoAXoECBUQAw&biw=1680&bih=947"
     articles_text = scrape(url)
-    context.bot.sendMessage(chat_id = context.job.context,text = (get_final_article(articles_text)))
-   # except:
-   #     context.bot.sendMessage(chat_id="-1001331038106", text="Error in weekly articles")
     
+    context.bot.sendMessage(chat_id = context.job.context,text = (get_final_article(articles_text)))
 
 
 def get_article(update,context):
     context.bot.sendMessage(chat_id = update.message.chat_id, text="Articles related to astronomy will be everyday.\n\n Clear Skies!")
     context.job_queue.run_daily(send_article, time = datetime.time(17,23,0,tzinfo=ind_tz), context = update.message.chat_id)
-    #context.job_queue.run_daily(send_article, time = datetime.time(17,23,0,tzinfo=ind_tz),days=(2,4,6), context = update.message.chat_id)
     
 def stop_func(update, context):
     context.bot.sendMessage(chat_id=update.message.chat_id, text='stopped')
@@ -129,7 +108,7 @@ def stop_func(update, context):
 
 # --------------------------------------------------------------------------------------------#
 
-# ----------------------- FETCH ARTICLE BASED ON KEYWORDS BY USER --------------------------- #
+# ----------------------- FETCH LATEST ARTICLE BASED ON KEYWORDS BY USER --------------------------- #
 def get_keyword_article(keyword):
     if(profanity.contains_profanity(keyword)):
         return "no results"
@@ -139,11 +118,9 @@ def get_keyword_article(keyword):
         #keyword = keyword.replace(" ","+")
         url = "https://www.google.com/search?q="+keyword+"&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjTi4TOw_7qAhWzyDgGHVjpAyYQ_AUoAXoECBUQAw&biw=1680&bih=948"
         articles_text = scrape(url)
-        return(get_final_article(articles_text))
-        #return url
+        return(get_final_article(articles_text)) #calls function from weekly articles generator
 
 def fetch_article(update, context):
-    chat_id = update.message.chat_id
     search_text=""
     for i in context.args:
         search_text = search_text + " " +i
@@ -151,9 +128,7 @@ def fetch_article(update, context):
         update.message.reply_text(get_keyword_article(search_text))
     except:
         update.message.reply_text("Error in retrieving data.")
-        context.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(fetch_article):\n" + str(sys.exc_info()))
-    
-#calls same function as weekly article generator    
+        context.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(fetch_article):\n" + str(sys.exc_info()))       
 
 # ----------------------------------------------------------------------------------------------#
 
@@ -161,11 +136,9 @@ def fetch_article(update, context):
 # ----------------------------- WIKI SUMMARY AND LINK GENERATOR --------------------------------#
 def get_wiki_link(search_text):
     google_url = "https://www.google.com/search?q="+search_text+"&rlz=1C5CHFA_enIN908IN908&oq="+search_text+"&aqs=chrome.0.69i59.6224j0j1&sourceid=chrome&ie=UTF-8"
-    google_page = requests.get(google_url)
-    google_page = bs4(google_page.text,'html.parser')
+    google_page = bs4((requests.get(google_url)).text,'html.parser')
     google_page = google_page.find(class_ = "ZINbbc xpd O9g5cc uUPGi")
-    wiki_link = google_page.find('a')['href'].replace('/url?q=','')
-    wiki_link = wiki_link.split("&sa")[0]
+    wiki_link = google_page.find('a')['href'].replace('/url?q=','').split('&sa')[0]
     if(wiki_link.find('wikipedia')):
         if(wiki_link.find("%25")):
             wiki_link = re.sub("%25","%", wiki_link)
@@ -202,7 +175,7 @@ def get_wiki_info(update, context):
     search_text=""
     for i in context.args:
         search_text = search_text + " " +i
-    search_text += "+wiki"
+    search_text += "+site:wikipedia.org"
     update.message.reply_text(scrape_wiki(search_text.lower()))
 
 # ----------------------------------------------------------------------------------------------#
@@ -219,21 +192,21 @@ def get_moon_info():
     return moon_image, moon_percent, moon_phase
 
 def weather_data(lat, lon, search_data):
+    
     openweather_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&exclude=minutely&units=metric"+"&appid=90701b1aba6e661af014c16e653b91c3"
     openweather_response = (requests.get(openweather_url)).json()
-    clearoutside_image = "https://clearoutside.com/forecast_image_medium/"+str(lat)+"/"+str(lon)+"/forecast.png"
+    
     sunset_time = datetime.datetime.fromtimestamp(int(openweather_response['current']['sunset'])).astimezone(ind_tz).time()
-    time = datetime.datetime.fromtimestamp(int(openweather_response['current']['dt'])).astimezone(ind_tz).time()
+    current_time = datetime.datetime.fromtimestamp(int(openweather_response['current']['dt'])).astimezone(ind_tz).time()
     cloud_cover = openweather_response['current']['clouds']
     wind_speed = round(float(openweather_response['current']['wind_speed']*18/5),2)
     description = openweather_response['current']['weather'][0]['description']
     dew_point  = openweather_response['current']['dew_point']
     temperature  = openweather_response['current']['temp']
-    icon_url = "http://openweathermap.org/img/wn/"+openweather_response['current']['weather'][0]['icon']+"@2x.png"
     moon_image, moon_percent, moon_phase = get_moon_info()
+
+    message = "Weather update for "+search_data+ "at " + str(current_time) +"\nStatus: "+description+"\nCloud cover: "+str(cloud_cover)+"%\nWind speed: "+str(wind_speed)+"kmph\nTemperature: "+str(temperature)+"°C\nDew Point: "+str(dew_point)+"°C\nMoon Illumination: "+moon_percent+"\nMoon phase: "+moon_phase
     
-    #message = str(time)
-    message = "Weather update for "+search_data+ "at " + str(time) +"\nStatus: "+description+"\nCloud cover: "+str(cloud_cover)+"%\nWind speed: "+str(wind_speed)+"kmph\nTemperature: "+str(temperature)+"°C\nDew Point: "+str(dew_point)+"°C\nMoon Illumination: "+moon_percent+"\nMoon phase: "+moon_phase
     return message, moon_image, clearoutside_image
     
 
@@ -241,7 +214,7 @@ def get_weather(update, context):
     search_text=""
     for i in context.args:
         search_text += i + ' '
-    #bot.sendMessage(chat_id=update.message.chat_id, text = search_text)    
+        
     if(re.search("^[0-9]",search_text)):
         lat, lon = search_text.replace(' ','').split(',')
     else:
@@ -253,11 +226,8 @@ def get_weather(update, context):
     try:    
         message, moon_photo, cl_image = weather_data(lat, lon, search_text)
         context.bot.sendPhoto(chat_id=update.message.chat_id, caption = message, photo=moon_photo)
-        #caching.clear_cache()
-        #context.bot.sendMessage(chat_id="-1001331038106", text = str(lat) + ", "+str(lon))
     except:
         context.bot.sendMessage(chat_id=update.message.chat_id, text="Error in retrieving data.")
-        #print(str(sys.exc_info()))
         context.bot.sendMessage(chat_id="-1001331038106", text = "AstroBot error(get_weather):\n" + str(sys.exc_info()))
 
 # ----------------------------------------------------------------------------------------------#
@@ -265,16 +235,34 @@ def get_weather(update, context):
 
 # ------------------------------------ PRINT HELP COMMANDS -------------------------------------#
 def bot_help(update, context):
-    chat_id = update.message.chat_id
-    help_text = "Hello, these are the commands I will respond to:\n\nTyping \"/randomarticle\" will fetch a random article related to an astronomy subject.\n\nTyping \"/newarticle <keyword>\" will fetch an article related to the keyword.\n\nTyping \"/wiki <keyword>\" will produce a short summary and link to wikipedia\n\nTyping \"/weather <latitude, longitude>\" or \"/weather <location name>\" will fetch a weather update.\n\nTyping \"/help\" will show you all the current list of commands I can respond to."
+    help_text = "Hello, these are the commands I will respond to:\n\n
+    Typing \"/randomarticle\" will fetch a random article related to an astronomy subject.\n\n
+    Typing \"/newarticle <keyword>\" will fetch an article related to the keyword.\n\n
+    Typing \"/wiki <keyword>\" will produce a short summary and link to wikipedia\n\n
+    Typing \"/weather <latitude, longitude>\" or \"/weather <location name>\" will fetch a weather update.\n\n
+    Typing \"/help\" will show you all the current list of commands I can respond to."
     update.message.reply_text(help_text)
     
+# ----------------------------------------------------------------------------------------------#
+
+# ------------------------------------ WELCOME NEW MEMBERS -------------------------------------#
+
+def welcome_new_user(update, context):
+    for new_user_obj in update.message.new_chat_members:
+        chat_id = update.message.chat_id
+        new_usr = ""
+        message="Welcome to HAC $user!" # Welcome message
+        try:
+            new_usr = '@' + new_user_obj['username']
+        except:
+            new_usr = new_user_obj['first_name']
+    context.bot.sendMessage(chat_id, text = message.replace('$user',new_usr))
+
 # ----------------------------------------------------------------------------------------------#
 
 
 
 def main():
-    caching.clear_cache()
     updater = Updater(token, use_context= True) 
     
     dp = updater.dispatcher
@@ -285,7 +273,7 @@ def main():
     dp.add_handler(CommandHandler('newarticle', fetch_article, pass_args=True))
     dp.add_handler(CommandHandler('wiki',get_wiki_info, pass_args=True))
     dp.add_handler(CommandHandler('weather',get_weather, pass_args=True))
-    caching.clear_cache()
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome_new_user))
     updater.start_polling()
     #updater.idle()
 
