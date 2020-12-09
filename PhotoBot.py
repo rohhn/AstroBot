b03a565f6b684fb33b9cb3ecb7abe64741fe6e05
Rohan <rohan@MacBook-Pro.local>
2020-12-09 16:13:18 +0530

    V2

--- PhotoBot.py
+++ PhotoBot.py
@@ -7,60 +7,53 @@ import hashlib
 import re
 from telegram.ext import Updater, CommandHandler, JobQueue
 
-token = open('config/photobot.conf','r').read()
-
 indt = pytz.timezone("Asia/Kolkata")
 
-def get_url(page_text):
-    if(page_text.find('img')):
-        url = "https://apod.nasa.gov/apod/"+page_text.find_all('center')[0].find('img')['src']
-        return(url)
-    elif(page_text.find('iframe')):
-        url = page_text.find_all('center')[0].find('iframe')['src']
-        return url
-    
-def get_title(page_text):
-    if(page_text.find('img')):
-        return(re.sub("  +"," ",page_text.find_all('center')[1].get_text().replace("\n"," ").split("Image")[0]))
-    elif(page_text.find('iframe')):
-        return(re.sub("  +"," ",page_text.find_all('center')[1].get_text().replace("\n"," ").split("Video")[0]))
-    
-def sendmessage(context):
-    url = "https://apod.nasa.gov/apod/astropix.html"
-    response = requests.get(url)
-    page_text = bs4(response.text,'html.parser')
-    date = page_text.find("p").contents[3].get_text().replace('\n','')
-    url = get_url(page_text)
-    title = get_title(page_text)
-    explanation = re.sub("  +"," ",page_text.find_all('p')[2].get_text().replace("\n"," ").split("Tomorrow")[0].split("Explanation: ")[1])
-    if(page_text.find('img')):
-        try:
-            context.bot.sendPhoto(chat_id=context.job.context, photo=url, caption = str("APOD - "+title+"\n\n"+explanation+"\nDate: " + date))
-        except:
+class PhotoBot():
+
+    def __init__(self):
+        return
+
+    def get_url(self, page_text):
+        if(page_text.find('img')):
+            url = "https://apod.nasa.gov/apod/"+page_text.find_all('center')[0].find('img')['src']
+            return(url)
+        elif(page_text.find('iframe')):
+            url = page_text.find_all('center')[0].find('iframe')['src']
+            return url
+        
+    def get_title(self, page_text):
+        if(page_text.find('img')):
+            return(re.sub("  +"," ",page_text.find_all('center')[1].get_text().replace("\n"," ").split("Image")[0]))
+        elif(page_text.find('iframe')):
+            return(re.sub("  +"," ",page_text.find_all('center')[1].get_text().replace("\n"," ").split("Video")[0]))
+        
+    def sendmessage(self, context):
+        url = "https://apod.nasa.gov/apod/astropix.html"
+        response = requests.get(url)
+        page_text = bs4(response.text,'html.parser')
+        date = page_text.find("p").contents[3].get_text().replace('\n','')
+        url = self.get_url(page_text)
+        title = self.get_title(page_text)
+        explanation = re.sub("  +"," ",page_text.find_all('p')[2].get_text().replace("\n"," ").split("Tomorrow")[0].split("Explanation: ")[1])
+        if(page_text.find('img')):
+            try:
+                context.bot.sendPhoto(chat_id=context.job.context, photo=url, caption = str("APOD - "+title+"\n\n"+explanation+"\nDate: " + date))
+            except:
+                message = ("<a href=\""+url+"\">" +"<b>APOD - " + title + "</b></a>\n\n" +"\n" +explanation+"\n<i>Date: "+date+"</i>\n")
+                context.bot.sendMessage(chat_id=context.job.context, text=message, parse_mode='HTML')
+        elif(page_text.find('iframe')):
             message = ("<a href=\""+url+"\">" +"<b>APOD - " + title + "</b></a>\n\n" +"\n" +explanation+"\n<i>Date: "+date+"</i>\n")
             context.bot.sendMessage(chat_id=context.job.context, text=message, parse_mode='HTML')
-    elif(page_text.find('iframe')):
-        message = ("<a href=\""+url+"\">" +"<b>APOD - " + title + "</b></a>\n\n" +"\n" +explanation+"\n<i>Date: "+date+"</i>\n")
-        context.bot.sendMessage(chat_id=context.job.context, text=message, parse_mode='HTML')
-    return()
+        return()
+        
     
-
-def daily_job(update, context):
-    context.bot.sendMessage(chat_id=update.message.chat_id, text="started")
-    context.job_queue.run_daily(sendmessage,time=datetime.time(11,23,0,tzinfo=indt), context=update.message.chat_id)
-    #job_queue.run_repeating(testing, 10, context=update)
-
-def stop_func(update, context):
-    context.bot.sendMessage(chat_id=update.message.chat_id,
-                      text='stopped')
-    context.job_queue.schedule_removal()
-
-def main():
-    updater = Updater(token, use_context= True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('startapod', daily_job, pass_job_queue=True))
-    dp.add_handler(CommandHandler('stopapod', stop_func, pass_job_queue=True))
-    updater.start_polling()
+    def daily_job(self, update, context):
+        context.bot.sendMessage(chat_id=update.message.chat_id, text="APOD started")
+        context.job_queue.run_daily(self.sendmessage,time=datetime.time(11,23,0,tzinfo=indt), context=update.message.chat_id)
+        #job_queue.run_repeating(testing, 10, context=update)
     
-if __name__ == '__main__':
-    main()
+    def stop_func(self, update, context):
+        context.bot.sendMessage(chat_id=update.message.chat_id, text='APOD stopped')
+        context.job_queue.schedule_removal()
+    
---
+++ main.py
@@ -0,0 +1,34 @@
+from AstBot import AstroBot
+from PhotoBot import PhotoBot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def main():
+    AstroBot_Token = open('config/astrobot.conf','r').read()
+    PhotoBot_Token = open('config/photobot.conf', 'r').read()
+
+    AstroBot_Updater = Updater(AstroBot_Token, use_context= True)
+    PhotoBot_Updater = Updater(PhotoBot_Token, use_context=True)
+
+
+    astrobot = AstroBot()
+    astrobot_dispatcher = AstroBot_Updater.dispatcher
+    astrobot_dispatcher.add_handler(CommandHandler('weekly_articles',astrobot.get_article, pass_job_queue = True))
+    astrobot_dispatcher.add_handler(CommandHandler('stop_weekly_articles',astrobot.stop_func, pass_job_queue = True))
+    astrobot_dispatcher.add_handler(CommandHandler('randomarticle',astrobot.random_article))
+    astrobot_dispatcher.add_handler(CommandHandler('help',astrobot.bot_help))
+    astrobot_dispatcher.add_handler(CommandHandler('news', astrobot.fetch_article, pass_args=True))
+    astrobot_dispatcher.add_handler(CommandHandler('wiki',astrobot.get_wiki_info, pass_args=True))
+    astrobot_dispatcher.add_handler(CommandHandler('weather',astrobot.get_weather, pass_args=True))
+    astrobot_dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, astrobot.welcome_new_user))
+    AstroBot_Updater.start_polling()
+
+    photobot = PhotoBot()
+    photobot_dispatcher = PhotoBot_Updater.dispatcher
+    photobot_dispatcher.add_handler(CommandHandler('startapod', photobot.daily_job, pass_job_queue=True))
+    photobot_dispatcher.add_handler(CommandHandler('stopapod', photobot.stop_func, pass_job_queue=True))
+    PhotoBot_Updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
