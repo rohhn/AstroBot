d01ab2f3b023056af59340100c10463dc7efe667
rohhn <rohan@MacBook-Pro.local>
2022-03-03 22:13:31 +0530

    Updated chrome options for weather command

--- hac_bot/astrobot.py
+++ hac_bot/astrobot.py
@@ -17,6 +17,7 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 ind_tz = pytz.timezone('Asia/Kolkata')
 
